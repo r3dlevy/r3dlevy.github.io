25b89c377a186cbb879c19ff327a621ab6b0fcc7
R3dLevy <juvy@live.fr>
2024-07-17 11:10:10 +0200

    First commit

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'R3dLevy |  Offsec Blog';
+export const SITE_DESCRIPTION = 'This blog primarily focuses on penetration testing, Red Teaming & C2, malware development, and other offensive security topics. I hope you will enjoy the content.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
